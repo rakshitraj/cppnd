37ab837c6ec577b36a32c428e659a2d430479e37
rakshitraj <rraj.rakshit@gmail.com>
2021-11-17 17:10:26 +0530

    reading from file

---
+++ introduction/reading_from_file/a.cpp
@@ -0,0 +1,16 @@
+// Reading from a file
+
+// C++ uses std::ifstream object to handle input file streams
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::ifstream my_file;
+    my_file.open("/home/raxit/cppnd/res/demo.txt");
+    if (my_file) {
+        std::cout << "The file stream is created" << "\n";
+    }
+}
