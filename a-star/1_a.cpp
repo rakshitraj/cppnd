77b468c5b5707cbf6dfe23520823bf615689a713
rakshitraj <rraj.rakshit@gmail.com>
2021-11-18 19:53:41 +0530

    modified : 1_a.cpp State()
    added additional, but not complete, funtionality to A* search

--- a-star/1_a.cpp
+++ a-star/1_a.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using std::cout;
 using std::ifstream;
@@ -12,7 +13,7 @@ using std::vector;
 using std::abs;
 
 // Declare an enumerated object that contains possible states in the gridworld
-enum class State {kEmpty, kObstacle, kClosed};
+enum class State {kPath, kEmpty, kObstacle, kClosed};
 
 
 // All funtion prototypes
@@ -22,6 +23,7 @@ std::string CellString (State);
 void PrintBoard (const std::vector<std::vector<State>>);
 int Heuristic (int, int, int, int);
 void AddToOpen (int, int, int, int, std::vector<std::vector<int>>&, std::vector<std::vector<State>>&);
+void CellSort(vector<vector<int>> *);
 std::vector<std::vector<State>> Search (std::vector<std::vector<State>>, int[2], int[2]);
 
 /**
@@ -70,13 +72,12 @@ std::vector<std::vector<State>> ReadBoardFile(std::string path) {
  * @param cell Cell of the gridworld
  * @return std::string 
  */
-std::string CellString(State cell){
-    switch (cell)
-    {
+string CellString(State cell) {
+  switch(cell) {
     case State::kObstacle: return "⛰️   ";
-        break;
-    default: return "0   ";
-    }
+    case State::kPath: return "🚗   ";
+    default: return "0   "; 
+  }
 }
 
 
@@ -128,6 +129,10 @@ bool Compare (std::vector<int> node1, std::vector<int> node2) {
     return false;
 }
 
+void CellSort(vector<vector<int>> *v) {
+    sort(v->begin(), v->end(), Compare);
+}
+
 /**
  * @brief Implements A* Search Algorithm
  * 
@@ -140,13 +145,28 @@ std::vector<std::vector<State>> Search (std::vector<std::vector<State>> grid, in
     
     // Create the vector of open nodes.
     vector<vector<int>> open {};
-  
-    // TODO: Initialize the starting node. 
-    open.push_back(std::vector<int> {init[0], init[1], 0, Heuristic(init[0], init[1], goal[0], goal[1])});
-  
-    // TODO: Use AddToOpen to add the starting node to the open vector.
-    AddToOpen(init[0], init[1], 0, Heuristic(init[0], init[1], goal[0], goal[1]), open, grid);
-
+    // Initialize the starting node. 
+    int x = init[0];
+    int y = init[1];
+    int g = 0;
+    int h = Heuristic(x, y, goal[0],goal[1]);
+    // Use AddToOpen to add the starting node to the open vector.
+    AddToOpen(x, y, g, h, open, grid);
+    // while open vector is non empty
+    while(open.size() > 0) {
+        // Sort open list using CellSort, and get the current node
+        CellSort(&open);
+        auto current = open.back();
+        open.pop_back();
+        // Get the x and y values from the current node
+        x = current[0];
+        y = current[1];
+        grid[x][y] = State::kPath;
+        // Check if agent has reached the goal. If so, return grid
+        if (x == goal[0] && y== goal[1])
+            return grid;
+    }
+    // We've run out of new nodes to explore and haven't found a path.
     cout<<"No path found" << '\n';
     return std::vector<std::vector<State>> {};
 }
